b926c2efba2805992be014e027b31fefb5c07ae4
Marcin Falkowski <marcin.falkowski@allegrogroup.com>
2014-12-14 16:29:43 +0100

    sshfs option to get rid of 'transport endpoint is not connected' error

--- features/sshfs/mount.sh
+++ features/sshfs/mount.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config
 
 cd ~
-sshfs -o reconnect $remote_host: $mount_local_dir
+sshfs -o reconnect -o ServerAliveInterval=15 $remote_host: $mount_local_dir
 cd -
