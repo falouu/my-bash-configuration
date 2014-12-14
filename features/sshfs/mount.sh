df8234a8a55d618c69226720869eaa87c10675b7
Marcin Falkowski <marcin.falkowski@allegrogroup.com>
2014-12-14 14:11:03 +0100

    bugfix for sshfs

--- features/sshfs/mount.sh
+++ features/sshfs/mount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source config
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/config
 
 cd ~
 sshfs -o reconnect $remote_host: $mount_local_dir
--- features/sshfs/umount.sh
+++ features/sshfs/umount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source config
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/config
 
 cd ~
 sudo fusermount -u $mount_local_dir
